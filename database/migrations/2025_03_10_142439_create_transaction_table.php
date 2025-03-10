79c7c8996fc4bea793e48f87f77cad6366c2bf43
Zenen Peraza <peraza@outlook.com>
2025-03-10 10:36:40 -0400

    10-03-25

---
+++ database/migrations/2025_03_10_142439_create_transaction_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type',['purchase', 'payment', 'sale', 'adjustment', 'refund']);            
+            $table->decimal('amount', 10, 2);
+            $table->bigInteger('reference_id')->nullable();
+            $table->text('description')->nullable();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
