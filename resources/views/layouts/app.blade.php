48045eab74e1799654be6918a2525672816ad3d1
Zenen Peraza <peraza@outlook.com>
2025-03-18 15:44:47 -0400

    18-03-25

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,3 @@
-<!doctype html>
 <html  lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none">
 
 <head>   
